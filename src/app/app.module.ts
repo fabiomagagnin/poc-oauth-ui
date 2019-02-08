706c7890af0623b4b7ead5c19b16b309bd927c6d
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-02-08 17:18:48 -0200

    Interceptor XRequestedWith: XMLHttpRequest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AppService } from './app.service';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login.component';
 import { AppComponent } from './app.component';
+import { XhrInterceptor } from './interceptor.module'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
@@ -26,7 +27,7 @@ const routes: Routes = [
     HttpClientModule,
     FormsModule
   ],
-  providers: [AppService],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
